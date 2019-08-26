37535df93a9634375b8b225eb9556e5ac5a0d7b2
xiedacon <xiedacon@qq.com>
2019-08-26 16:14:56 +0800

    chore: append doc

--- index.d.ts
+++ index.d.ts
@@ -36,40 +36,363 @@ export interface Logger {
 }
 
 export interface Transaction {
+  /**
+   * Add a create operation with given path, data, acls and mode.
+   *
+   * @param {string} path The znode path.
+   * @param {string|Buffer=} [data] The data buffer.
+   * @param {Array<Jute.data.ACL>=} acl An array of ACL object.
+   * @param {number=} flags The creation mode.
+   */
   create(path: string, data?: string | Buffer, acl?: Array<ACL>, flags?: number): this;
+  /**
+   * Add a check (existence) operation with given path and optional version.
+   *
+   * @param {string} path The znode path.
+   * @param {number=} version The version of the znode.
+   */
   check(path: string, version?: number): this;
+  /**
+   * Add a set-data operation with the given path, data and optional version.
+   *
+   * @param {string} path The znode path.
+   * @param {string|Buffer=} data The data buffer.
+   * @param {number=} version The version of the znode.
+   */
   setData(path: string, data?: string | Buffer, version?: number): this;
+  /**
+   * Add a delete operation with the given path and optional version.
+   *
+   * @param {string} path The znode path.
+   * @param {number=} version The version of the znode.
+   */
   remove(path: string, version?: number): this;
+  /**
+   * Execute the transaction atomically.
+   */
   commit(): Promise<{ header: { type: number; done: boolea; err: number; }, payload: object }>;
 }
 
 export class Client {
   constructor(connectionString: string, options?: Options);
-  ready(): Promise<void>;
+  /**
+   * The session id for this ZooKeeper client instance. The value returned is
+   * not valid until the client connects to a server and may change after a
+   * re-connect.
+   */
   get getSessionId(): Buffer;
+  /**
+   * The session password for this ZooKeeper client instance. The value
+   * returned is not valid until the client connects to a server and may
+   * change after a re-connect.
+   */
   get getSessionPassword(): Buffer;
+  /**
+   * The negotiated session timeout for this ZooKeeper client instance. The
+   * value returned is not valid until the client connects to a server and
+   * may change after a re-connect.
+   */
   get getSessionTimeout(): number;
+  /**
+   * Start the client and try to connect to the ensemble.
+   */
   connect(): Promise<void>;
+  /**
+   * Close this client object. Once the client is closed, its session becomes
+   * invalid. All the ephemeral nodes in the ZooKeeper server associated with
+   * the session will be removed. The watches left on those nodes (and on
+   * their parents) will be triggered.
+   */
   close(): Promise<void>;
+  /**
+   * Create a node with the given path. The node data will be the given data,
+   * and node acl will be the given acl.
+   *
+   * The flags argument specifies whether the created node will be ephemeral
+   * or not.
+   *
+   * An ephemeral node will be removed by the ZooKeeper automatically when the
+   * session associated with the creation of the node expires.
+   *
+   * The flags argument can also specify to create a sequential node. The
+   * actual path name of a sequential node will be the given path plus a
+   * suffix "i" where i is the current sequential number of the node. The sequence
+   * number is always fixed length of 10 digits, 0 padded. Once
+   * such a node is created, the sequential number will be incremented by one.
+   *
+   * If a node with the same actual path already exists in the ZooKeeper, a
+   * KeeperException with error code KeeperException.NodeExists will be
+   * thrown. Note that since a different actual path is used for each
+   * invocation of creating sequential node with the same path argument, the
+   * call will never throw "file exists" KeeperException.
+   *
+   * If the parent node does not exist in the ZooKeeper, a KeeperException
+   * with error code KeeperException.NoNode will be thrown.
+   *
+   * An ephemeral node cannot have children. If the parent node of the given
+   * path is ephemeral, a KeeperException with error code
+   * KeeperException.NoChildrenForEphemerals will be thrown.
+   *
+   * This operation, if successful, will trigger all the watches left on the
+   * node of the given path by exists and getData API calls, and the watches
+   * left on the parent node by getChildren API calls.
+   *
+   * If a node is created successfully, the ZooKeeper server will trigger the
+   * watches on the path left by exists calls, and the watches on the parent
+   * of the node by getChildren calls.
+   *
+   * The maximum allowable size of the data array is 1 MB (1,048,576 bytes).
+   * Arrays larger than this will cause a KeeperExecption to be thrown.
+   *
+   * @param {string} path the path for the node
+   * @param {string|Buffer=} data the initial data for the node
+   * @param {Array<Jute.data.ACL>=} acl the acl for the node
+   * @param {number=} flags specifying whether the node to be created is ephemeral and/or sequential
+   */
   create(path: string, data?: string | Buffer, acl?: Array<ACL>, flags?: number): Promise<{ path: string }>;
+  /**
+   * Create a node with the given path and returns the Stat of that node. The
+   * node data will be the given data and node acl will be the given acl.
+   *
+   * The flags argument specifies whether the created node will be ephemeral
+   * or not.
+   *
+   * An ephemeral node will be removed by the ZooKeeper automatically when the
+   * session associated with the creation of the node expires.
+   *
+   * The flags argument can also specify to create a sequential node. The
+   * actual path name of a sequential node will be the given path plus a
+   * suffix "i" where i is the current sequential number of the node. The sequence
+   * number is always fixed length of 10 digits, 0 padded. Once
+   * such a node is created, the sequential number will be incremented by one.
+   *
+   * If a node with the same actual path already exists in the ZooKeeper, a
+   * KeeperException with error code KeeperException.NodeExists will be
+   * thrown. Note that since a different actual path is used for each
+   * invocation of creating sequential node with the same path argument, the
+   * call will never throw "file exists" KeeperException.
+   *
+   * If the parent node does not exist in the ZooKeeper, a KeeperException
+   * with error code KeeperException.NoNode will be thrown.
+   *
+   * An ephemeral node cannot have children. If the parent node of the given
+   * path is ephemeral, a KeeperException with error code
+   * KeeperException.NoChildrenForEphemerals will be thrown.
+   *
+   * This operation, if successful, will trigger all the watches left on the
+   * node of the given path by exists and getData API calls, and the watches
+   * left on the parent node by getChildren API calls.
+   *
+   * If a node is created successfully, the ZooKeeper server will trigger the
+   * watches on the path left by exists calls, and the watches on the parent
+   * of the node by getChildren calls.
+   *
+   * The maximum allowable size of the data array is 1 MB (1,048,576 bytes).
+   * Arrays larger than this will cause a KeeperExecption to be thrown.
+   *
+   * @param {string} path the path for the node
+   * @param {string|Buffer=} data the initial data for the node
+   * @param {Array<Jute.data.ACL>=} acl the acl for the node
+   * @param {number=} flags specifying whether the node to be created is ephemeral and/or sequential
+   * @param {Buffer=} ttl specifying a TTL when mode is CreateMode.PERSISTENT_WITH_TTL or CreateMode.PERSISTENT_SEQUENTIAL_WITH_TTL
+   */
   create2(path: string, data?: string | Buffer, acl?: Array<ACL>, flags?: number, ttl?: Buffer): Promise<{ path: string; stat: Stat }>;
+  /**
+   * Delete the node with the given path. The call will succeed if such a node
+   * exists, and the given version matches the node's version (if the given
+   * version is -1, it matches any node's versions).
+   *
+   * A KeeperException with error code KeeperException.NoNode will be thrown
+   * if the nodes does not exist.
+   *
+   * A KeeperException with error code KeeperException.BadVersion will be
+   * thrown if the given version does not match the node's version.
+   *
+   * A KeeperException with error code KeeperException.NotEmpty will be thrown
+   * if the node has children.
+   *
+   * This operation, if successful, will trigger all the watches on the node
+   * of the given path left by exists API calls, and the watches on the parent
+   * node left by getChildren API calls.
+   *
+   * @param {string} path the path of the node to be deleted
+   * @param {number=} version the expected node version
+   */
   delete(path: string, version?: number): Promise<void>;
+  /**
+   * Set the data for the node of the given path if such a node exists and the
+   * given version matches the version of the node (if the given version is
+   * -1, it matches any node's versions). Return the stat of the node.
+   *
+   * This operation, if successful, will trigger all the watches on the node
+   * of the given path left by getData calls.
+   *
+   * A KeeperException with error code KeeperException.NoNode will be thrown
+   * if no node with the given path exists.
+   *
+   * A KeeperException with error code KeeperException.BadVersion will be
+   * thrown if the given version does not match the node's version.
+   *
+   * The maximum allowable size of the data array is 1 MB (1,048,576 bytes).
+   * Arrays larger than this will cause a KeeperException to be thrown.
+   *
+   * @param {string} path the path of the node
+   * @param {string|Buffer} data the data to set
+   * @param {number=} version the expected matching version
+   */
   setData(path: string, data?: string | Buffer, version?: number): Promise<{ stat: Stat }>;
+  /**
+   *
+   * Return the data and the stat of the node of the given path.
+   *
+   * If the watch is non-null and the call is successful (no exception is
+   * thrown), a watch will be left on the node with the given path. The watch
+   * will be triggered by a successful operation that sets data on the node, or
+   * deletes the node.
+   *
+   * A KeeperException with error code KeeperException.NoNode will be thrown
+   * if no node with the given path exists.
+   *
+   * @param {string} path the node path
+   * @param {(event: { type: number, state: number, path: string }) => any=} watcher explicit watcher
+   */
   getData(path: string, watcher?: (event: { type: number, state: number, path: string }) => any): Promise<{ data: Buffer, stat: Stat }>;
+  /**
+   * Set the ACL for the node of the given path if such a node exists and the
+   * given aclVersion matches the acl version of the node. Return the stat of the
+   * node.
+   *
+   * A KeeperException with error code KeeperException.NoNode will be thrown
+   * if no node with the given path exists.
+   *
+   * A KeeperException with error code KeeperException.BadVersion will be
+   * thrown if the given aclVersion does not match the node's aclVersion.
+   *
+   * @param {string} path the given path for the node
+   * @param {Array<Jute.data.ACL>} acl the given acl for the node
+   * @param {number=} version the given acl version of the node
+   */
   setACL(path: string, acl: Array<ACL>, version?: number): Promise<{ stat: Stat }>;
+  /**
+   * Retrieve the ACL list and the stat of the node of the given path.
+   *
+   * @param {string} path The node path.
+   */
   getACL(path: string): Promise<{ acl: Array<ACL>; stat: Stat }>;
+  /**
+   * Return the stat of the node of the given path. Return null if no such a
+   * node exists.
+   *
+   * If the watch is non-null and the call is successful (no exception is thrown),
+   * a watch will be left on the node with the given path. The watch will be
+   * triggered by a successful operation that creates/delete the node or sets
+   * the data on the node.
+   *
+   * @param {string} path the node path
+   * @param {(event: { type: number, state: number, path: string }) => any=} watcher explicit watcher
+   */
   exists(path: string, watcher?: (event: { type: number, state: number, path: string }) => any): Promise<{ stat: Stat }>;
+  /**
+   * Return the list of the children of the node of the given path.
+   *
+   * If the watch is non-null and the call is successful (no exception is thrown),
+   * a watch will be left on the node with the given path. The watch will be
+   * triggered by a successful operation that deletes the node of the given
+   * path or creates/delete a child under the node.
+   *
+   * The list of children returned is not sorted and no guarantee is provided
+   * as to its natural or lexical order.
+   *
+   * A KeeperException with error code KeeperException.NoNode will be thrown
+   * if no node with the given path exists.
+   *
+   * @param {string} path the node path
+   * @param {(event: { type: number, state: number, path: string }) => any=} watcher explicit watcher
+   */
   getChildren(path: string, watcher?: (event: { type: number, state: number, path: string }) => any): Promise<{ children: Array<string> }>;
+  /**
+   * For the given znode path return the stat and children list.
+   *
+   * If the watch is non-null and the call is successful (no exception is thrown),
+   * a watch will be left on the node with the given path. The watch will be
+   * triggered by a successful operation that deletes the node of the given
+   * path or creates/delete a child under the node.
+   *
+   * The list of children returned is not sorted and no guarantee is provided
+   * as to its natural or lexical order.
+   *
+   * A KeeperException with error code KeeperException.NoNode will be thrown
+   * if no node with the given path exists.
+   *
+   * @param {string} path the node path
+   * @param {(event: { type: number, state: number, path: string }) => any=} watcher explicit watcher
+   */
   getChildren2(path: string, watcher?: (event: { type: number, state: number, path: string }) => any): Promise<{ children: Array<string>, stat: Stat }>;
+  /**
+   * Gets all numbers of children nodes under a specific path
+   *
+   * @param {string} path
+   */
   getAllChildrenNumber(path: string): Promise<{ totalNumber: number }>;
+  /**
+   * Gets all the ephemeral nodes matching prefixPath
+   * created by this session.  If prefixPath is "/" then it returns all
+   * ephemerals
+   *
+   * @param {string} prefixPath
+   */
   getEphemerals(prefixPath?: string): Promise<{ ephemerals: Array<string> }>;
+  /**
+   * Flushes channel between process and leader.
+   *
+   * @param {string} path
+   */
   sync(path: string): Promise<{ path: string }>;
+  /**
+   * A Transaction is a thin wrapper on the multi method
+   * which provides a builder object that can be used to construct
+   * and commit an atomic set of operations.
+   */
   transaction(): Transaction;
   getChildWatches(): Array<string>;
   getDataWatches(): Array<string>;
   getExistWatches(): Array<string>;
+  /**
+   * For the given znode path, removes the specified watcher of given
+   * watcherType.
+   *
+   * Watcher shouldn't be null. A successful call guarantees that, the
+   * removed watcher won't be triggered.
+   *
+   * @param {string} path the path of the node
+   * @param {(event: { type: number, state: number, path: string }) => any} watcher explicit watcher
+   * @param {number} type the type of watcher to be removed
+   */
   removeWatches(path: string, watcher: (event: { type: number, state: number, path: string }) => any, type: number): Promise<void>;
+  /**
+   * For the given znode path, removes all the registered watchers of given
+   * watcherType.
+   *
+   * A successful call guarantees that, the removed watchers won't be
+   * triggered.
+   *
+   * @param {string} path the path of the node
+   * @param {number} type the type of watcher to be removed
+   */
   removeAllWatches(path: string, type: number): Promise<void>;
+  /**
+   * Return the last committed configuration (as known to the server to which the client is connected)
+   * and the stat of the configuration.
+   *
+   * If the watch is non-null and the call is successful (no exception is
+   * thrown), a watch will be left on the configuration node (ZooDefs.CONFIG_NODE). The watch
+   * will be triggered by a successful reconfig operation
+   *
+   * A KeeperException with error code KeeperException.NoNode will be thrown
+   * if the configuration node doesn't exists.
+   *
+   * @param {(event: { type: number, state: number, path: string }) => any=} watcher explicit watcher
+   */
   getConfig(wathcer?: (event: { type: number, state: number, path: string }) => any): Promise<{ data: Buffer; stat: Stat }>
 }
 
